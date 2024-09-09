b1c76428ae4d7bb90961e3254344e4334d9113f6
SanchitAgrawal2405 <anubhaon786@gmail.com>
2024-09-09 09:59:54 +0530

    Add files via upload

---
+++ WEEK1/project 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num1,num2,i ;
+	cout<<"Enter first number:";
+	cin>>num1;
+	cout<<"Enter second number:";
+	cin>>num2;
+	for (i=0;i<num2;i++){
+		num1++;
+	}
+	cout<<"Sum of two number is:"<<num1;
+	return 0;
+	
+	
+}
