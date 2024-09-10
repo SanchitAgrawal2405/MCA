01aa54b385685c78eb909694f437f6622863509e
SanchitAgrawal2405 <anubhaon786@gmail.com>
2024-09-10 09:50:15 +0530

    Add files via upload

---
+++ WEEK5/sum of array elements.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n,arr[n],sum=0;
+	cout<<"Enter the number of elements you want to enter in array:";
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	for(int i=0;i<n;i++){
+		sum+=arr[i];
+	}
+	cout<<"The sum of elements of array is : "<<sum<<endl;
+	return 0;
+}
